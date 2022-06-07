be1d2293148e3953e70c3e54777c16c40b6e6251
Coralel <coralel@post.bgu.ac.il>
2022-06-07 20:26:40 +0300

    update 7/6/22

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
+from flask import url_for
+from flask import render_template
+from datetime import timedelta
+from flask import request, session, jsonify
+app = Flask(__name__)
+
+app.secret_key = '123'
+app.config['SESSION_PERMANENT'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=20)
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/contactUs')
+def goToContacUs():
+    return render_template('ContactUs.html')
 
+@app.route('/')
+def redirectHomePage():
+    return redirect(url_for('goToHomePage'))
+
+@app.route('/homePage')
+def goToHomePage():
+    return render_template('HomePage.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

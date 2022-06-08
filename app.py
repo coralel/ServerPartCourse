861fda084d3a858c66c8dcc631764106daa95c08
Coralel <coralel@post.bgu.ac.il>
2022-06-08 16:54:32 +0300

    delete

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+# import
 from flask import Flask, redirect, render_template
 from flask import url_for
-from flask import render_template
 from datetime import timedelta
 from flask import request, session, jsonify
+
+# Create a Flask instance
 app = Flask(__name__)
+
+
+# Create a route decorator
+@app.route('/')
+def ContactUs():
+    return render_template("ContactUs.html")
+
 
 app.secret_key = '123'
 app.config['SESSION_PERMANENT'] = True
@@ -11,22 +20,27 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=20)
 
 app = Flask(__name__)
 
+
 # https://www.youtube.com/watch?v=4yaG-jFfePc
 @app.route('/contactUs')
 def PageName(pageName):
     return render_template('PageName.html', name=pageName)
 
+
 @app.route('/contactUs')
-def goToContacUs():
+def goToContactUs():
     return render_template('ContactUs.html')
+
 
 @app.route('/')
 def redirectHomePage():
     return redirect(url_for('goToHomePage'))
 
+
 @app.route('/homePage')
 def goToHomePage():
     return render_template('HomePage.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
